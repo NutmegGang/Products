86a40d198acae1e88c962013020c1b9069554517
Will Atwood <x1@galaxia.us>
2022-04-22 16:59:38 -0400

    /Styles endpoint now online and functional.  Server complete.

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { sql, products, product, pool, otherProducts} = require('../../database/index.js')
+const { products, product, related, styles } = require('../../database/index.js')
 
 
 const router = Router();
@@ -7,26 +7,9 @@ const router = Router();
 //we are in the /products route so only need to use /
 router.get('/', (req,res, next) => {
   const {page, count} = req.query;
-
-  // products()
-  // .then(data => res.json(data))
-  // .catch(err => console.log(err))
-  // otherProducts((err, response) => {
-  //   if(err) {
-  //     console.log("Error", err)
-  //     res.send(500)
-  //   } else {
-  //     res.send(response)
-  //   }
-  // })
   products(page, count)
   .then(data => res.status(200).json(data.rows))
   .catch(err => res.status(500).send(err))
-
-  // pool.query('SELECT id, name, slogan, description, category, default_price FROM public."Product_name" ORDER BY id ASC LIMIT 4', (err, ress) => {
-  //   if(err) return console.log(err);
-  //   res.json(ress.rows)
-  //   })
 })
 
 router.get('/:product_id', (req,res, next) => {
@@ -36,13 +19,18 @@ router.get('/:product_id', (req,res, next) => {
   .catch(err => console.warn(err))
 })
 
-router.get('/:product_id/styles', (req,res, next) => {
-  res.status(200).send('Styles', req.params)
-})
-
 router.get('/:product_id/related', (req,res, next) => {
-  res.status(200).send('Related', req.params)
+  const { product_id } = req.params
+  related(product_id)
+  .then(data => res.status(200).json(data))
+  .catch(err => res.status(500).json(err))
 })
 
+router.get('/:product_id/styles', (req,res, next) => {
+  const { product_id } = req.params
+  styles(product_id)
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json(err))
+})
 
 module.exports = router;
